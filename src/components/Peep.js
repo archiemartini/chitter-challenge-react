287cb0809fb470ca47823b1aee8a2d289d45a012
archiemartini <archie.cgm@gmail.com>
2022-08-08 09:54:06 +0100

    refactored date time function

--- src/components/Peep.js
+++ src/components/Peep.js
@@ -1,54 +1,14 @@
 import React from "react";
+import dateTimeFormatter from "../helper_modules/dateFormatter";
 
 export default function Peep(props) {
 
-  const dateFormat = props.created_at.slice(0, 10)
-  const timeFormat = props.created_at.slice(11, 16)
-
-  function dateTimeFormatter(props) {
-    const date = props.created_at.slice(0, 10)
-    const time = props.created_at.slice(11, 16)
-  
-    const monthName = (dateString) => {
-      let month = dateString.slice(5, 7)
-      if (month[0] === "0") {
-        month = month[1]
-       };
-
-      let calendar = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-      return calendar[month - 1]
-    } 
-    const ordinalDate = (dateString) => {
-     let dayNum = dateString.slice(8, 10)
-
-     if (dayNum[0] === "0") {
-      dayNum = dayNum[1]
-     };
-
-    let selector;
-
-     if (dayNum <= 0) {
-      selector = 4;
-    } else if ((dayNum > 3 && dayNum < 21) || dayNum % 10 > 3) {
-      selector = 0;
-    } else {
-      selector = dayNum % 10;
-    }
-  
-    return dayNum + ['th', 'st', 'nd', 'rd', ''][selector];
-
-    }
-    
-  return `${monthName(date)} ${ordinalDate(date)}`
-  }
-  
-  dateTimeFormatter(props)
 
   return (
     <div className="peep">
       <div>
         <span className="peep--username">{props.user.handle} </span>
-        <span>{dateTimeFormatter(props)}</span>
+        <span>{dateTimeFormatter(props.created_at)}</span>
       </div>
       <div>
         <span className="peep--bodytext">"{props.body}"</span>
--- src/helper_modules/dateFormatter.js
+++ src/helper_modules/dateFormatter.js
@@ -1,8 +1,35 @@
-export default function dateTimeFormatter(dateTimeString) {
+export default  function dateTimeFormatter(dateTimeString) {
   const date = dateTimeString.slice(0, 10)
-  const time = dateTimeString.slice(11, 16)
 
-  const ordinalDate = (date) => {
-   const dayNum = date.slice() 
+  const monthName = (dateString) => {
+    let month = dateString.slice(5, 7)
+    if (month[0] === "0") {
+      month = month[1]
+     };
+
+    let calendar = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    return calendar[month - 1]
+  } 
+  const ordinalDate = (dateString) => {
+   let dayNum = dateString.slice(8, 10)
+
+   if (dayNum[0] === "0") {
+    dayNum = dayNum[1]
+   };
+
+  let selector;
+
+   if (dayNum <= 0) {
+    selector = 4;
+  } else if ((dayNum > 3 && dayNum < 21) || dayNum % 10 > 3) {
+    selector = 0;
+  } else {
+    selector = dayNum % 10;
   }
+
+  return dayNum + ['th', 'st', 'nd', 'rd', ''][selector];
+
+  }
+  
+return `${monthName(date)} ${ordinalDate(date)}`
 }
