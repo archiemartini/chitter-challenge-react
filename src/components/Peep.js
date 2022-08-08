a2913a1c280c757a9d93a53d0c5eeccba093b705
archiemartini <archie.cgm@gmail.com>
2022-08-08 10:23:58 +0100

    heart dynamically changes colour

--- src/components/Peep.js
+++ src/components/Peep.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import dateTimeFormatter from "../helper_modules/dateFormatter";
 
 export default function Peep(props) {
 
+  const [isLiked, setIsLiked] = useState(false)
+
+  
+  const setHeartColour = (boolean) => {
+    if (boolean === false) {
+      return {color: "gray"}
+    } else {
+      return {color: "red"}
+    }
+  }
+
+  const handleHeartClick = () => {
+    setIsLiked(!isLiked)
+  }
 
   return (
     <div className="peep">
@@ -14,7 +28,8 @@ export default function Peep(props) {
         <span className="peep--bodytext">"{props.body}"</span>
       </div>
       <div>
-        <span className="peep--likes">Likes: {props.likes.length}</span>
+      <i class="fa-solid fa-heart" style={setHeartColour(isLiked)} onClick={handleHeartClick}></i>
+        <span className="peep--likes"> {props.likes.length}</span>
       </div>
     </div>
   )
