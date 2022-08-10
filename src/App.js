af062da2c19ddc87e34c298b7af822008522723e
archiemartini <archie.cgm@gmail.com>
2022-08-10 17:03:32 +0100

    login is working and redirecting to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Route, Routes} from "react-router-dom"
+import React, {useState} from "react"
+import { Route, Routes, Redirect} from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import SignUp from "./components/SignUp"
@@ -7,13 +7,17 @@ import Login from "./components/Login"
 
 export default function App() {
 
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [userData, setUserData] = useState({ user_id:"", session_key:"", username:"" })
+
+  console.log(userData)
 
   return (
     <>
-     <Navbar />
+     <Navbar loggedIn={loggedIn}/>
      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home userData={userData} />} />
+        <Route path='/login' element={<Login setLoggedIn={setLoggedIn} setUserData={setUserData}/>} />
         <Route path='/signup' element={<SignUp />} />
      </Routes>
     </>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,62 @@
-import React from "react"
+import React, {useState} from "react"
+import { useNavigate } from "react-router-dom"
+import api from '../api/base'
 
-export default function Login() {
+export default function Login(props) {
+
+  const nav = useNavigate()
+
+  const [loginUsername, setLoginUsername] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+  const loginData = {
+    "session": {
+      "handle":`${loginUsername}`, "password":`${loginPassword}`
+    }
+  }
+
+  const [loginFailed, setLoginFailed] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(loginData)
+
+    try {
+      const response = await api.post('/sessions', loginData)
+      console.log(response.data)
+      response.data.username = loginUsername
+      props.setLoggedIn(true)
+      props.setUserData(response.data)
+      nav("/")
+      
+    } catch (err) {
+      console.log(`Error: ${err.message}`)
+      setLoginFailed(true)
+    }
+    
+  }
 
   return (
     <>
       <h1 className="header">Login</h1>
-      <form className="login-form">
-        <input type='text' placeholder="username"></input>
-        <input type='text' placeholder="password"></input>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input 
+          type='text' 
+          name="text"
+          placeholder="username"
+          required
+          value={loginUsername}
+          onChange={e => setLoginUsername(e.target.value)}
+        />
+        <input 
+          type='password'
+          name="password" 
+          placeholder="password"
+          required
+          value={loginPassword}
+          onChange={e => setLoginPassword(e.target.value)}
+
+        />
+        <input type='submit' value="Login" />
       </form>   
     </>
   )
